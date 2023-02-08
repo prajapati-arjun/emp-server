43425bf3cd6cfa1d06c71ddda8f2fa51a0470439
Arjun Prajapati <73653551+prajapati-arjun@users.noreply.github.com>
2023-02-08 13:40:37 +0530

    Update db.js

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 mongoose.set('strictQuery', true);
 const Connection = async (username, password) => {
-    const URL = `mongodb://user:codewitharjun@ac-gkmwrph-shard-00-00.13i0nyj.mongodb.net:27017,ac-gkmwrph-shard-00-01.13i0nyj.mongodb.net:27017,ac-gkmwrph-shard-00-02.13i0nyj.mongodb.net:27017/?ssl=true&replicaSet=atlas-zb5mem-shard-0&authSource=admin&retryWrites=true&w=majority`
+    const URL = `mongodb://${username}:${password}@ac-bv8j9ge-shard-00-00.odxjpgx.mongodb.net:27017,ac-bv8j9ge-shard-00-01.odxjpgx.mongodb.net:27017,ac-bv8j9ge-shard-00-02.odxjpgx.mongodb.net:27017/?ssl=true&replicaSet=atlas-u9umui-shard-0&authSource=admin&retryWrites=true&w=majority`
 
     try {
         // 1 - Current URL string parser is deprecated, and will be removed in a future version. 
